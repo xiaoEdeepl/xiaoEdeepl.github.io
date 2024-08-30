00b2094f5a572ac1e1ae334fd58a4989a218d61a
xiaoEdeepl <912052978@qq.com>
2024-08-31 01:33:26 +0800

    Site updated: 2024-08-31 01:33:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/31/hello-world/"];function toRandomPost(){
+var posts=["2024/08/31/hello-world/","2024/08/31/2024-7-28周报/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
