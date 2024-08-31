834b9333d3aa0d7010aaf876ef8cbc2cc2f6471b
xiaoEdeepl <912052978@qq.com>
2024-08-31 15:29:35 +0800

    Site updated: 2024-08-31 15:29:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/31/2024-7-28周报/","2024/08/31/2024-8-11周报/","2024/08/31/hello-world/"];function toRandomPost(){
+var posts=["2024/08/31/2024-7-28周报/","2024/08/31/2024-8-25周报/","2024/08/31/2024-8-11周报/","2024/08/31/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
